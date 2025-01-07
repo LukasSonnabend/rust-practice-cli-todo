a7726713565b01e06ecd0c9eb2ca088271b1b990
Lukas Sonnabend <lukas@gograb.au>
2025-01-07 23:11:27 +0100

    feat(remove todo)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod tasks;
 mod file_system;
+use std::ops::ControlFlow;
+
 use tasks::Task;
 use file_system::{load_from_file, save_to_file};
-use clap::{arg, Command};
+use clap::{arg, Arg, Command};
 use chrono::{DateTime, Duration, Utc};
 
 fn cli() -> Command {
@@ -15,6 +17,15 @@ fn cli() -> Command {
                 .about("Add a new TODO")
                 .arg(arg!(<text> "Text of the Todo"))
                 
+        )
+        .subcommand(
+            Command::new("remove")
+            .about("Remove a todo")
+            .arg(
+                Arg::new("index")
+                    .required(false)
+                    .index(1),
+            )
         )
 }
 
@@ -32,10 +43,56 @@ fn main() {
             });
             println!("Added new TODO: {}", text);
         },
+        Some(("remove", remove_matches)) => {
+            if let ControlFlow::Break(_) = fun_name(&mut todos, remove_matches) {
+                save_to_file(&todos).expect("Failed to save todos");
+                return;
+            }
+        },
         _ => {
             println!("No subcommand provided");
         }
     };
     save_to_file(&todos).expect("Failed to save todos");
 
+}
+
+fn fun_name(todos: &mut Vec<Task>, remove_matches: &clap::ArgMatches) -> ControlFlow<()> {
+    let index = remove_matches.get_one::<String>("index");
+    match index {
+        Some(index_string) => {
+            // Pattern match e.g. unpack the result of the parse method
+            if let Ok(index_parsed) = index_string.parse::<usize>() {
+                if index_parsed >= todos.len() {
+                    println!("Index out of bounds");
+                    return ControlFlow::Break(());
+                }
+                todos.remove(index_parsed);
+            }
+        },
+        None => {
+            println!("Enter index of todo to remove:");
+            for (i, todo) in todos.iter().enumerate() {
+                println!("{}: {}", i, todo.name);
+            }
+            let mut input = String::new();
+            std::io::stdin().read_line(&mut input).expect("Failed to read line");
+            match input.trim().parse::<usize>() {
+                Ok(index) => {
+                    if index >= todos.len() {
+                        println!("Index out of bounds");
+                        return ControlFlow::Break(());
+                    }
+                    todos.remove(index);
+                },
+                Err(_) => {
+                    println!("Invalid index");
+                    return ControlFlow::Break(());
+                }
+            }
+        }
+    
+    }
+    
+    ControlFlow::Continue(())
 }
